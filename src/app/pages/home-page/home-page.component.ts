53bd0cc65fa65908439f73c43d88fb630ed2fc79
Alok <alokkumar@pinelane.in>
2019-09-15 02:04:19 +0530

    Triple search field is left

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -67,6 +67,22 @@ export class HomePageComponent implements OnInit {
       else if(!this.skills && this.experience && !this.location && !this.companyname) this.singleDataFiler("experience", this.experience)
       else if(!this.skills && !this.experience && this.location && !this.companyname) this.singleDataFiler("location", this.location)
       else if(!this.skills && !this.experience && !this.location && this.companyname) this.singleDataFiler("company", this.companyname)
+      //CHECK FOR TWO NON-EMPTY FIELDS: SKILLS & EXPERIENCE -> SKILLS & LOCATION -> SKILLS & COMPANY
+      else if(this.skills && this.experience && !this.location && !this.companyname){
+        this.doubleDataFilter("skills", "experience", this.skills, this.experience)
+      }else if(this.skills && !this.experience && this.location && !this.companyname){
+        this.doubleDataFilter("skills", "location", this.skills, this.location)
+      }else if(this.skills && !this.experience && !this.location && this.companyname){
+        this.doubleDataFilter("skills", "company", this.skills, this.companyname)
+      }// EXPERIENCE & LOCATION -> EXPERIENCE & COMPANY
+      else if(!this.skills && this.experience && this.location && !this.companyname){
+        this.doubleDataFilter("experience", "location", this.experience, this.location)
+      }else if(!this.skills && this.experience && !this.location && this.companyname){
+        this.doubleDataFilter("experience", "company", this.skills, this.companyname)
+      }// LOCATION & COMPANY
+      else if(!this.skills && !this.experience && this.location && this.companyname){
+        this.doubleDataFilter("location", "company", this.location, this.companyname)
+      }
     }
   }
 
@@ -104,6 +120,43 @@ export class HomePageComponent implements OnInit {
         if(log){
           //SEARCHING FOR THE TERM OPERATION
           if(log.search(new RegExp(text, "i")) != -1) filtered.push(job)
+        }
+      }
+      this.postFilter(filtered)
+    })
+  }
+
+  /* DOUBLE DATA CHECK */
+  doubleDataFilter(item1, item2, text1, text2){
+    var log1, log2
+    this.loading = true
+    this.http.get('https://nut-case.s3.amazonaws.com/jobs.json').subscribe(data => {
+      let filtered = [] //for populating filtered data
+      for(let job of cast(data['data'], Job)){
+        if(item1 == "skills" && item2 == "experience"){
+          log1 = job.skills; 
+          log2 = job.experience
+        }else if(item1 == "skills" && item2 == "location"){
+          log1 = job.skills; 
+          log2 = job.location
+        }else if(item1 == "skills" && item2 == "company"){
+          log1 = job.skills; 
+          log2 = job.companyname
+        }else if(item1 == "experience" && item2 == "location"){
+          log1 = job.experience; 
+          log2 = job.location
+        }else if(item1 == "experience" && item2 == "company"){
+          log1 = job.experience; 
+          log2 = job.companyname
+        }else{
+          log1 = job.location; 
+          log2 = job.companyname
+        }
+
+        if(log1 && log2){
+          //SEARCHING FOR THE TERM OPERATION
+          if(log1.search(new RegExp(text1, "i")) != -1 && log2.search(new RegExp(text2, "i")) != -1) 
+            filtered.push(job)
         }
       }
       this.postFilter(filtered)
