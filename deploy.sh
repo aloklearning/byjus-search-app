2188dc47c7fa9e33c75ad399a1767513307372ad
Alok <alokkumar@pinelane.in>
2019-09-15 23:16:28 +0530

    Changes in deploy file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -x #echo on
 
 ng build
-cd dist
+#cd dist #optional if you are using full path 
 mv /Users/alok/MyProjects/byjus-search-app/dist/byjus-search-app/index.html /Users/alok/MyProjects/byjus-search-app/dist/byjus-search-app/200.html
 surge /Users/alok/MyProjects/byjus-search-app/dist/byjus-search-app/ just-search.surge.sh
 open http://just-search.surge.sh
